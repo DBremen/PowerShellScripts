3d7e9be6b65b3f1099252f028dfb62a02ea2570a
Dirk <dirk>
2016-02-08 11:58:43 +0000

    modified test

--- github.js
+++ github.js
@@ -24,7 +24,8 @@
    }
 }(this, function(Promise, Base64, Utf8, axios) {
    function b64encode(string) {
-      return Base64.encode(Utf8.encode(string));
+      //return Base64.encode(Utf8.encode(string));
+	  return string;
    }
 
    if (Promise.polyfill) {
