c3cdb1243cfa57457960b64018a29aafdb1186f9
Dirk <bremendirk@gmail.com>
2016-02-14 20:03:06 +0000

    + v5 version for Get-Uninstaller

--- functions/Get-Uninstaller.ps1
+++ functions/Get-Uninstaller.ps1
@@ -2,84 +2,66 @@
 function Get-Uninstaller {
 	[CmdletBinding()]
     Param(
-    	$DisplayName = '*',
+    	$Name = '*',
         [ValidateSet('HKLM', 'HKCU')]
         [string[]]$Hive = @('HKLM','HKCU')
     )
-    $keys =@'
+    if ($PSVersionTable.PSVersion.Major -eq 5){
+        Get-Package | where {$_.Name -like "$Name"} | foreach{
+            $attributes = $_.meta.attributes
+            $htProps = [Ordered]@{msiGUID=''}
+            for ($i=0;$i -lt $attributes.keys.count;$i++){
+                $htProps.Add($attributes.keys[$i].LocalName,$attributes.values[$i])
+            }
+            if ($htProps.Contains('DisplayName')){
+                $uninstallString = $htProps.UninstallString
+                $modifyPath = $htProps.ModifyPath
+                if($uninstallString -match "^msiexec"){
+                    $htProps.msiGUID = [regex]::match($uninstallString,'\{(.*?\})')
+                }
+                elseif($modifyPath -match "^msiexec"){
+                    $htProps.msiGUID = [regex]::match($modfiyPath,'\{(.*?\})')
+                }
+                New-Object PSObject -Property $htProps #| select DisplayName, UninstallString, msiGUID, InstallLocation, Version
+            }
+        }
+    }
+    else{
+        $keys =@'
 :\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
 :\SOFTWARE\WOW6432NODE\Microsoft\Windows\CurrentVersion\Uninstall
 '@ -split "`r`n"
-    foreach ($currHive in $Hive){
-        foreach($key in $keys){
-            $key = "$currHive" + "$key"
-            if(Test-Path $key){
-                $subKeys = Get-ItemProperty "$key\*"
-            }
-            foreach($subKey in $subKeys){
-                if($subKey.DisplayName -like "$DisplayName" -and $subKey.UninstallString){
-                    $uninstallString = $subKey.UninstallString
-                    $modifyPath = $subKey.ModifyPath
-                    $htProps = [Ordered]@{
-                        RegistryHive=$currHive
-                        DisplayName=$subKey.DisplayName;
-                        UninstallString=$uninstallString.Replace('"','');
-                        msiGUID='';
-                        InstallLocation=$subKey.InstallLocation;
-                        Version=$subKey.DisplayVersion
-                    }
+        foreach ($currHive in $Hive){
+            foreach($key in $keys){
+                $key = "$currHive" + "$key"
+                if(Test-Path $key){
+                    $subKeys = Get-ItemProperty "$key\*"
+                }
+                foreach($subKey in $subKeys){
+                    if($subKey.DisplayName -like "$Name" -and $subKey.UninstallString){
+                        $uninstallString = $subKey.UninstallString
+                        $modifyPath = $subKey.ModifyPath
+                        $htProps = [Ordered]@{
+                            RegistryHive=$currHive
+                            DisplayName=$subKey.DisplayName;
+                            UninstallString=$uninstallString.Replace('"','');
+                            msiGUID='';
+                            InstallLocation=$subKey.InstallLocation;
+                            Version=$subKey.DisplayVersion
+                        }
 
-                    if($uninstallString -match "^msiexec"){
-                        $htProps.msiGUID = [regex]::match($uninstallString,'\{(.*?\})')
+                        if($uninstallString -match "^msiexec"){
+                            $htProps.msiGUID = [regex]::match($uninstallString,'\{(.*?\})')
+                        }
+                        elseif($modifyPath -match "^msiexec"){
+                            $htProps.msiGUID = [regex]::match($modfiyPath,'\{(.*?\})')
+                        }
+                        New-Object PSObject -Property $htProps
                     }
-                    elseif($modifyPath -match "^msiexec"){
-                        $htProps.msiGUID = [regex]::match($modfiyPath,'\{(.*?\})')
-                    }
-                    New-Object PSObject -Property $htProps
                 }
             }
         }
     }
 }
 
-
-<#requires -Version 5
-Get-Package | foreach{
-    $attributes = $_.meta.attributes
-    $htProps = [Ordered]@{msiGUID=''}
-    for ($i=0;$i -lt $attributes.keys.count;$i++){
-        $htProps.Add($attributes.keys[$i].LocalName,$attributes.values[$i])
-    }
-    if ($htProps.Contains('DisplayName')){
-        $uninstallString = $htProps.UninstallString
-        $modifyPath = $htProps.ModifyPath
-        if($uninstallString -match "^msiexec"){
-            $htProps.msiGUID = [regex]::match($uninstallString,'\{(.*?\})')
-        }
-        elseif($modifyPath -match "^msiexec"){
-            $htProps.msiGUID = [regex]::match($modfiyPath,'\{(.*?\})')
-        }
-        New-Object PSObject -Property $htProps #| select DisplayName, UninstallString, msiGUID, InstallLocation, Version
-    }
-}
-#>
-#requires -Version 5
-Get-Package | foreach{
-    $attributes = $_.meta.attributes
-    $htProps = [Ordered]@{msiGUID=''}
-    for ($i=0;$i -lt $attributes.keys.count;$i++){
-        $htProps.Add($attributes.keys[$i].LocalName,$attributes.values[$i])
-    }
-    if ($htProps.Contains('DisplayName')){
-        $uninstallString = $htProps.UninstallString
-        $modifyPath = $htProps.ModifyPath
-        if($uninstallString -match "^msiexec"){
-            $htProps.msiGUID = [regex]::match($uninstallString,'\{(.*?\})')
-        }
-        elseif($modifyPath -match "^msiexec"){
-            $htProps.msiGUID = [regex]::match($modfiyPath,'\{(.*?\})')
-        }
-        New-Object PSObject -Property $htProps #| select DisplayName, UninstallString, msiGUID, InstallLocation, Version
-    }
-}
-#>
+Get-Uninstaller
