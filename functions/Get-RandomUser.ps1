8b3cf67595848424b377f753c9c3ff5853e316f2
Dirk Bremen <Dirk_Bremen@symantec.com>
2016-05-19 08:16:16 +0100

    Adding Get-RandomUser and Pester-Reporting

---
+++ functions/Get-RandomUser.ps1
@@ -0,0 +1,62 @@
+﻿function Get-RandomUser {
+    <#
+        .SYNOPSIS
+            Generate random user data.
+        .DESCRIPTION
+            This function uses the free API for generating random user data from https://randomuser.me/
+        .EXAMPLE
+            Get-RandomUser 10
+        .EXAMPLE
+            Get-RandomUser -Amount 25 -Nationality us,gb -Format csv -ExludeFields picture
+        .LINK
+            https://randomuser.me/
+        .NOTES
+            Author: Øyvind Kallstad
+            Date: 08.04.2016
+            Version: 2.0
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [ValidateRange(1,5000)]
+        [alias('Results')]
+        [int] $Amount,
+
+        [Parameter()]
+        [ValidateSet('Male','Female')]
+        [string] $Gender,
+
+        # Pictures won't be affected by this, but data such as location, cell/home phone, id, etc. will be more appropriate.
+        [ValidateSet('AU','BR','CA','CH','DE','DK','ES','FI','FR','GB','IE','IR','NL','NZ','TR','US')]
+        [alias('Nat')]
+        [string[]] $Nationality,
+
+        # Seeds allow you to always generate the same set of users.
+        [int] $Seed,
+
+        [ValidateSet('json','csv','yaml','xml')]
+        [string] $Format = 'json',
+
+        # Fields to include in the results.
+        [ValidateSet('gender','name','location','email','login','registered','dob','phone','cell','id','picture','nat')]
+        [alias('Inc')]
+        [string[]] $IncludeFields,
+
+        # Fields to exclude from the the results.
+        [ValidateSet('gender','name','location','email','login','registered','dob','phone','cell','id','picture','nat')]
+        [alias('Exc')]
+        [string[]] $ExcludeFields
+    )
+    $rootUrl = "http://api.randomuser.me/?format=$($Format)"
+    $htParamAlias = (Get-Command Get-RandomUser).Parameters.Values | Where-Object Aliases | 
+        Select-Object Name, Aliases | Group-Object Name -AsHashTable -AsString
+    $PSBoundParameters.GetEnumerator() | foreach {
+        $value = $_.Value -join ','
+        $key = $_.Key
+        if ($htParamAlias.ContainsKey($key)){
+            $key = $htParamAlias.$Key.Aliases.ToLower()
+        }
+        $rootUrl += "&$key=$value"
+    }
+    Invoke-RestMethod -Uri $rootUrl
+}
---
+++ functions/Pester-Reporting.ps1
@@ -0,0 +1,40 @@
+﻿$tempFolder = New-Item "$env:Temp\PesterTest" -ItemType Directory -force
+foreach ($num in (1..10)){
+    $functionTemplate =  @"
+function Test$num {
+    $(if ($Num -eq 8){
+        "$($num + 1)"
+        }else{
+        "$num"
+        }
+    )
+}
+"@ | Set-Content -Path (Join-Path $tempFolder "Test$num.ps1")
+    $testTemplate = @"
+`$here = Split-Path -Parent `$MyInvocation.MyCommand.Path
+`$sut = (Split-Path -Leaf `$MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "`$here\`$sut"
+Describe "Test$num" {
+    It "Should output $num" {
+        Test$num | Should Be $num
+    }
+}
+"@ | Set-Content -Path (Join-Path $tempFolder "Test$num.Tests.ps1")
+}
+Push-Location $tempFolder
+Invoke-Pester -OutputFile report.xml -OutputFormat NUnitXml
+#display overall test-suite results
+$testResults
+#display specific tests within test suite
+$testResults.TestResult
+
+#download and extract ReportUnit.exe
+$url = 'http://relevantcodes.com/Tools/ReportUnit/reportunit-1.2.zip'
+$fullPath = Join-Path $tempFolder $url.Split("/")[-1]
+(New-Object Net.WebClient).DownloadFile($url,$fullPath)
+(New-Object -ComObject Shell.Application).Namespace($tempFolder.FullName).CopyHere((New-Object -ComObject Shell.Application).Namespace($fullPath).Items(),16)
+del $fullPath
+
+#run reportunit against report.xml and display result in browser
+& .\reportunit.exe report.xml
+ii report.html
