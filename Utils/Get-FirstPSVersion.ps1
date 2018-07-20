3f10516ff8be447c7520ade3336c30959c4fe5f8
Dirk <bremendirk@gmail.com>
2018-07-20 19:52:20 +0100

    Added Get-FirstPSVersion

---
+++ Utils/Get-FirstPSVersion.ps1
@@ -0,0 +1,9 @@
+﻿function Get-FirstPSVersion ($command){
+    $baseUri = 'https://docs.microsoft.com/en-us/powershell/module/Microsoft.PowerShell.Archive'
+	foreach ($version in ('3.0','4.0','5.0','5.1','6')){
+	    $url = (Invoke-WebRequest -uri ("$baseUri/$command" + "?view=powershell-$version") -MaximumRedirection 0 -ea SilentlyContinue).Headers.Location
+	    if ($url -notlike '*FallbackFrom*'){
+            return [Version]$version
+        }
+    }
+}
